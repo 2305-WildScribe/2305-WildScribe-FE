b8a14430657544cd838de092f59bfb9934d56874
Alice Abarca <aliceabarca@otmail.com>
2023-10-11 17:46:43 -0600

    Feat: add new components to app page

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,10 +1,19 @@
 import './App.scss';
 import Homepage from '../Homepage/Homepage';
+import NavBar from '../NavBar/NavBar';
+import Adventure from '../Adventure/Adventure';
+import AdventureContainer from '../AdventureContainer/AdventureContainer';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Homepage />
+      <NavBar />
+      <Routes> 
+        <Route path='/' element={<Homepage /> } /> 
+        <Adventure />
+        <AdventureContainer />
+      </Routes>
     </div>
   );
 }
