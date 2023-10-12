cd3b80eb8d7499412314d2611def3cda44e4584a
Alice Abarca <aliceabarca@otmail.com>
2023-10-11 18:04:05 -0600

    Feat: add interface to determine data sets

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -4,15 +4,32 @@ import NavBar from '../NavBar/NavBar';
 import Adventure from '../Adventure/Adventure';
 import AdventureContainer from '../AdventureContainer/AdventureContainer';
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+
+interface Adventures {
+  activity: string;
+  date: string;
+  notes: string;
+  image_url: string;
+  stress_level: string;
+  hydration: number;
+  diet: string;
+  avdenture_id: number;
+}
+
+interface AppState {
+  userName: string;
+  adventures: Adventures[];
+}
 
 function App() {
+  const [adventures, setAdventures] = useState<AppState['adventures']>()
+  
   return (
     <div className='App'>
       <NavBar />
       <Routes> 
-        <Route path='/' element={<Homepage /> } /> 
-        <Adventure />
-        <AdventureContainer />
+        <Route path='/' element={<Homepage /> } />
       </Routes>
     </div>
   );
