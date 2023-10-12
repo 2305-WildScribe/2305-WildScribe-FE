043906a081f39baad17bab287944ffe364019114
Jwensloff <61986277+Jwensloff@users.noreply.github.com>
2023-10-12 12:21:09 -0600

    Update App.tsx
    feat: Remove unused component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,7 +1,6 @@
 import './App.scss';
 import Homepage from '../Homepage/Homepage';
 import NavBar from '../NavBar/NavBar';
-import AdventureContainer from '../AdventureContainer/AdventureContainer';
 import { Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import postAdventure from '../../apiCalls';
