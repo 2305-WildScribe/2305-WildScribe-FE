cfe3a3c75a1bce318d446aa9d4b49a272ab61ee8
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-12 12:26:28 -0600

    feat: Add a passing cypress test

--- cypress/e2e/homepage_spec.cy.ts
+++ cypress/e2e/homepage_spec.cy.ts
@@ -2,6 +2,6 @@
 
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/')
     cy.url().should('contain', 'localhost:3000')})
   })
