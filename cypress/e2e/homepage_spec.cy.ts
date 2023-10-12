f5d0403311380902eb746d2a43bccd90ef6ed94e
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-12 12:18:21 -0600

    feat: Add a cypress test to try and get github actions working

--- cypress/e2e/homepage_spec.cy.ts
+++ cypress/e2e/homepage_spec.cy.ts
@@ -1,5 +1,7 @@
+/// <reference types="cypress" />
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
+    cy.url().should('contain', 'localhost:3000')})
   })
-})
