a62b8435ab85c4fb0c5c62cc0743c2a0ec085044
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-13 14:00:29 -0600

    feat: Add logo and h1 to nav bar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,10 +1,15 @@
 import './NavBar.scss';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../../Assets/logo.png';
 
 function NavBar(): React.ReactElement {
   return (
     <nav className='nav-bar'>
+      <div className='nav-text-wrapper'>
+        <img className='logo' src={logo} />
+        <h1>WildScribe</h1>
+      </div>
       <div className='nav-button-container'>
         <NavLink className='home-btn nav-link' to='/'>
           Home
