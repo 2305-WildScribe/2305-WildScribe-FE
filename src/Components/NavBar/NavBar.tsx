80ba3d8383a5c387ace12c284195930a33639adf
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-14 16:10:40 -0600

    feat: Add margins around Nav Bar components and adventure card wrapper

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -7,14 +7,14 @@ function NavBar(): React.ReactElement {
   return (
     <nav className='nav-bar'>
       <div className='nav-text-wrapper'>
-        <img className='logo' src={logo} />
+        <img className='logo' src={logo} alt='WildScribe logo of an open book in front of a mountain range'/>
         <h1>WildScribe</h1>
       </div>
       <div className='nav-button-container'>
         <NavLink className='home-btn nav-link' to='/'>
           Home
         </NavLink>
-        <NavLink className='nav-link' to='/logAdventure'> 
+        <NavLink className='new-adventure-btn nav-link' to='/logAdventure'> 
         Log New Adventure
       </NavLink>
       </div>
