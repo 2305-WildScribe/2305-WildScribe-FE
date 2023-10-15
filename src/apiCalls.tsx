8ac36e3b8ace6eed94d22f36046ce5729a5bb624
Alice Abarca <aliceabarca@otmail.com>
2023-10-15 13:52:23 -0600

    feat: Add new post api call

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,10 +1,6 @@
-// import  Adventure  from '../../../types';
+import Adventure from "../types";
 
-// interface AdventureContainerProps {
-//   adventures: Adventure[];
-// }
-
-async function postAdventure() {
+export async function postAdventure() {
   try {
     const response = await fetch(
       'https://117105e4-6093-4d95-8632-31f93d58b35a.mock.pstmn.io/api/v0/adventures',
@@ -28,4 +24,29 @@ async function postAdventure() {
   } catch (error) {}
 }
 
-export default postAdventure;
+export async function postNewAdventure(newAdventureData: Adventure) {
+  try {
+    const response = await fetch(
+      'https://117105e4-6093-4d95-8632-31f93d58b35a.mock.pstmn.io/api/v0/user/adventures',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data: {
+            type: 'adventures',
+            user_id: 12,
+            attributes: {
+              newAdventureData
+            },
+          },
+        }),
+      }
+    );
+    if (!response.ok) {
+      throw new Error('error');
+    }
+    return await response.json();
+  } catch (error) {}
+}
