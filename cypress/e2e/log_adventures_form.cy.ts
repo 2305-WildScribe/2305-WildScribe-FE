7cdfa259ebc31c571bf1b497c478fcd9831f44d5
Alice Abarca <aliceabarca@otmail.com>
2023-10-16 11:32:44 -0600

    feat: Add test for log adventure form

---
+++ cypress/e2e/log_adventures_form.cy.ts
@@ -0,0 +1,36 @@
+describe('log adventure form', () => {
+
+  beforeEach(() => {
+    cy.fixture('dummy_data.json').then((response) => {
+      cy.intercept(
+        'POST',
+        'https://117105e4-6093-4d95-8632-31f93d58b35a.mock.pstmn.io/api/v0/adventures',
+        {
+          statusCode: 201,
+          body: {
+            activity: 'White Water Rafting',
+            date: '10/14/2023',
+            notes: 'Medium water was great',
+            image_url: 'https://www.rei.com/adventures/assets/adventures/images/trip/gallery/northamerica/crh_12',
+            stress_level: 'Moderate',
+            hydration: 'Hydrated',
+            diet: 'Average',
+            sleep: 'Average',
+            extraDietNotes: 'More Protein',
+            extraSleepNotes: 'At least 8 hours instead of 6 hours next time',
+          }
+        }
+      )
+    });
+    cy.visit('http://localhost:3001/logAdventure');
+  });
+
+  it('should contain log adventure components', () => {
+    cy.url().should('contain', 'localhost:3001/logAdventure')
+    cy.get('[for="activity-input"]').contains('Activity')
+    cy.get('[name="activity"]').type('White Water Rafting')
+      .should('have.value', 'White Water Rafting')
+    cy.get('[for="date-input"]').should('be.visible')
+    cy.get('[type="date"]').click().type('2023-10-14')
+  })
+})
