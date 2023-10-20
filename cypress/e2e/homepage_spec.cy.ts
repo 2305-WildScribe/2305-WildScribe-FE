01dde0d621ddaf684d44ffacfd090b3868929246
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-20 14:10:23 -0600

    feat: Stub the first 2 api calls

--- cypress/e2e/homepage_spec.cy.ts
+++ cypress/e2e/homepage_spec.cy.ts
@@ -1,32 +1,67 @@
 /// <reference types="cypress" />
 
 describe('Homepage', () => {
-
   beforeEach(() => {
+    // cy.fixture('userInfo.json').then((response)=>{
+    cy.intercept(
+      'POST',
+      'https://safe-refuge-07153-b08bc7602499.herokuapp.com/api/v0/user',
+      {
+        statusCode: 201,
+        fixture: 'userInfo.json',
+      }
+    ).as('login');
+    // })
+
+    // cy.intercept(
+    //   'POST',
+    //   'https://safe-refuge-07153-b08bc7602499.herokuapp.com/api/v0/user/adventures',
+    //   {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       data: {
+    //         type: 'adventures',
+    //         attributes: {
+    //           user_id: '12',
+    //         },
+    //       },
+    //     }),
+    //   }
+    // ).as('postUserId');
+
     cy.fixture('dummy_data.json').then((response) => {
       cy.intercept(
         'POST',
-        'https://117105e4-6093-4d95-8632-31f93d58b35a.mock.pstmn.io/api/v0/adventures',
+        'https://safe-refuge-07153-b08bc7602499.herokuapp.com/api/v0/user/adventures',
         {
           statusCode: 201,
           body: response,
         }
-      ).as('postUser');
+      ).as('getUserData');
+      console.log('response --->', response);
     });
     cy.visit('http://localhost:3000/');
   });
 
   it('should contain the homepage components', () => {
+    // cy.wait('@postUserId');
     cy.url().should('contain', 'localhost:3000');
-    cy.wait('@postUser');
-    cy.get('.nav-bar').should('exist');
+    cy.get('.login-form').should('exist');
+    cy.get('.login-btn').should('exist');
+    cy.get('.login-btn').click();
+    cy.wait('@login');
+    cy.wait('@getUserData')
     cy.get('.nav-button-container').should('exist');
-    cy.get('.home-btn').should('have.class', 'active')
+    // cy.get('.adventure-card-container').should('exist')
 
-    cy.get('.adventure-card-container').should('exist')
-    cy.get('.adventure-card').should('have.length', 5)
-    cy.get('.adventure-card').first().should('have.id', 1)
-    cy.get('.adventure-card').last().should('have.id', 5)
+    // cy.get('.nav-bar').should('exist');
+    // cy.get('.home-btn').should('have.class', 'active')
 
+    // cy.get('.adventure-card').should('have.length', 5)
+    // cy.get('.adventure-card').first().should('have.id', 1)
+    // cy.get('.adventure-card').last().should('have.id', 5)
   });
 });
--- src/Components/LoginPage/LoginPage.tsx
+++ src/Components/LoginPage/LoginPage.tsx
@@ -33,7 +33,8 @@ function LoginPage({
       const userId = response.data.attributes.user_id;
       setUserId(userId);
       localStorage.setItem('UserId', JSON.stringify(userId));
-      retrieveUserInformation(userId);
+      console.log('is',userId)
+      retrieveUserInformation(userId)
       localStorage.setItem('UserId', JSON.stringify(true));
       console.log('isLoggedIn', isLoggedIn);
       navigate('/home');
@@ -66,7 +67,7 @@ function LoginPage({
             required
           />
 
-          <button type='submit' onClick={(e) => handleLogin(e)}>
+          <button className='login-btn' type='submit' onClick={(e) => handleLogin(e)}>
             Login
           </button>
         </div>
