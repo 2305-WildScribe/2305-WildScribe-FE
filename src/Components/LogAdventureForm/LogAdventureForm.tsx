d0d7a62871aef53c9e1461e8e94413f92e77a7ca
Jocelyn Wensloff <jewensloff@gmail.com>
2023-10-22 16:23:53 -0600

    feat: Add styling to the log components

--- src/Components/LogAdventureForm/LogAdventureForm.tsx
+++ src/Components/LogAdventureForm/LogAdventureForm.tsx
@@ -134,7 +134,7 @@ function LogAdventureForm({
               </button>
             </div>
           </div>
-          <p>Over the last 48 hours, how would you describe the following:</p>
+          <p className='user-prompt'>Over the last 48 hours, how would you describe the following:</p>
           <div className='second-line-components'>
             <select
               name='stressLevel'
