7c4c3cd86bfe558217fb953b937eb2debdf07f0e
Jocelyn Wensloff <jewensloff@gmail.com>
2023-11-24 17:11:23 -0700

    feat: Add the last log date on the Journal cards

--- src/Components/Journal/Journal.tsx
+++ src/Components/Journal/Journal.tsx
@@ -1,21 +1,51 @@
-import './Journal.scss'
-import { useNavigate } from 'react-router-dom'
+import { useAppSelector } from '../../Redux/hooks';
+import { selectAdventures } from '../../Redux/slices/adventuresSlice';
+import { Adventure } from '../../types';
+import './Journal.scss';
+import { useNavigate } from 'react-router-dom';
+import dayjs from 'dayjs';
 
 interface JournalProps {
-  activity: string
+  activity: string;
 }
 
-function Journal({activity}: JournalProps) {
-  const navigate = useNavigate()
+function Journal({ activity }: JournalProps) {
+  const navigate = useNavigate();
+
   const handleJournalClick = () => {
-    navigate(`/${activity}`)
-  }
+    navigate(`/${activity}`);
+  };
+
+  let adventures = useAppSelector(selectAdventures).adventures;
+
+  console.log('activity', activity);
+
+  const lastLog = () => {
+    let filteredLogs = adventures.filter(
+      (adventure) => adventure.activity === activity
+    );
+
+    const latest = filteredLogs.reduce(
+      (accum, adventure) => {
+        const adventureDate = dayjs(adventure.date);
+        const accumDate = dayjs(accum.date);
+        if (adventureDate.isAfter(accumDate)) {
+          accum = adventure;
+        }
+        return accum;
+        console.log(accum);
+      },
+      { date: '0' }
+    );
+    return dayjs(latest.date).format('MM-DD-YYYY');
+  };
 
   return (
     <div className='activity-card' onClick={() => handleJournalClick()}>
       {activity}
+      <div className='last-log'>Last log: {lastLog()}</div>
     </div>
-  )
+  );
 }
 
-export default Journal
+export default Journal;
