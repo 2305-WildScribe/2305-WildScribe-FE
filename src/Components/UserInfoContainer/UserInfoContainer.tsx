7e9d0f5d2f196e4823c1e45ae3cd05549f711154
Jocelyn Wensloff <jewensloff@gmail.com>
2023-11-24 21:15:18 -0700

    feat: Display activity data in the pie chart

--- src/Components/UserInfoContainer/UserInfoContainer.tsx
+++ src/Components/UserInfoContainer/UserInfoContainer.tsx
@@ -6,6 +6,7 @@ import { PieChart } from '@mui/x-charts/PieChart';
 
 function UserInfoContainer() {
   let adventures = useAppSelector(selectAdventures).adventures;
+  let activityTypes = useAppSelector(selectAdventures).activityTypes;
 
   function findMostRecentLog() {
     let sorted = adventures?.slice().sort((a, b) => {
@@ -20,17 +21,31 @@ function UserInfoContainer() {
   const mostRecentDate = dayjs(mostRecent?.date)?.format('MM-DD-YYYY');
   const mostRecentActivity = findMostRecentLog()?.activity;
 
+  const pieChartData = () => {
+    const types = activityTypes.map((type, index) => {
+      let data = { id: index, value: 0, label: '' };
+      data.label += type;
+      adventures.forEach((adventure) => {
+        if (adventure.activity === type) {
+          data.value += 1;
+        }
+      });
+      return data;
+    });
+    return types;
+  };
+
+  console.log(pieChartData());
+  const palette = ['#01204E', '#028391', '#115C80', '#008081', '#479685' ];
+  
   return (
     <div className='user-info-wrapper'>
       Your last log was on {mostRecentDate} in your {mostRecentActivity} journal
       <PieChart
+        colors={palette}
         series={[
           {
-            data: [
-              { id: 0, value: 10, label: 'series A' },
-              { id: 1, value: 15, label: 'series B' },
-              { id: 2, value: 20, label: 'series C' },
-            ],
+            data: pieChartData(),
           },
         ]}
         width={400}
