cfe9eaff622c18cdf2b4d8261d82d4bbc028a3c7
Jocelyn Wensloff <jewensloff@gmail.com>
2023-11-24 20:42:03 -0700

    feat: Display a pie chart on the dashboard

--- src/Components/UserInfoContainer/UserInfoContainer.tsx
+++ src/Components/UserInfoContainer/UserInfoContainer.tsx
@@ -2,6 +2,7 @@ import './UserInfoContainer.scss';
 import { useAppSelector } from '../../Redux/hooks';
 import { selectAdventures } from '../../Redux/slices/adventuresSlice';
 import dayjs from 'dayjs';
+import { PieChart } from '@mui/x-charts/PieChart';
 
 function UserInfoContainer() {
   let adventures = useAppSelector(selectAdventures).adventures;
@@ -16,12 +17,25 @@ function UserInfoContainer() {
   }
 
   const mostRecent = findMostRecentLog();
-  const mostRecentDate = dayjs(mostRecent.date).format('MM-DD-YYYY');
-  const mostRecentActivity = findMostRecentLog().activity;
+  const mostRecentDate = dayjs(mostRecent?.date)?.format('MM-DD-YYYY');
+  const mostRecentActivity = findMostRecentLog()?.activity;
 
   return (
     <div className='user-info-wrapper'>
       Your last log was on {mostRecentDate} in your {mostRecentActivity} journal
+      <PieChart
+        series={[
+          {
+            data: [
+              { id: 0, value: 10, label: 'series A' },
+              { id: 1, value: 15, label: 'series B' },
+              { id: 2, value: 20, label: 'series C' },
+            ],
+          },
+        ]}
+        width={400}
+        height={200}
+      />
     </div>
   );
 }
